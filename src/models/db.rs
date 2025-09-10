cbd522111116d318f8d7f374bfaa2334a54937a3
adam-cakrda <cakrda@outlook.cz>
2025-09-10 19:21:18 +0200

    Fix second time database ids and dates

--- src/models/db.rs
+++ src/models/db.rs
@@ -33,46 +33,36 @@ where
 
 #[derive(Serialize, Deserialize, Debug, Clone, utoipa::ToSchema)]
 pub struct Repository {
-    #[schema(value_type = String, example = "665f1f5a2ab79b9f2ff6a0d1")]
     #[serde(serialize_with = "serialize_object_id_as_hex_string")]
     pub _id: ObjectId,
-    #[schema(value_type = String, example = "665f1f5a2ab79b9f2ff6a0d0")]
     #[serde(serialize_with = "serialize_object_id_as_hex_string")]
     pub user: ObjectId, // owner
     pub name: String,
     pub description: String,
     pub is_private: bool,
-    #[schema(value_type = Option<String>)]
     #[serde(serialize_with = "serialize_option_object_id_as_hex_string")]
     pub forked_from: Option<ObjectId>,
-    #[schema(value_type = String, format = DateTime, example = "2024-01-01T12:00:00Z")]
     #[serde(serialize_with = "serialize_bson_datetime_as_rfc3339_string")]
     pub created_at: DateTime,
-    #[schema(value_type = String, format = DateTime, example = "2024-01-01T12:00:00Z")]
     #[serde(serialize_with = "serialize_bson_datetime_as_rfc3339_string")]
     pub updated_at: DateTime,
 }
 
 #[derive(Serialize, Deserialize, Debug, utoipa::ToSchema)]
 pub struct Token {
-    #[schema(value_type = String)]
     #[serde(serialize_with = "serialize_object_id_as_hex_string")]
     pub _id: ObjectId,
-    #[schema(value_type = String)]
     #[serde(serialize_with = "serialize_object_id_as_hex_string")]
     pub user: ObjectId,
     pub token: String,
-    #[schema(value_type = String, format = DateTime)]
     #[serde(serialize_with = "serialize_bson_datetime_as_rfc3339_string")]
     pub created_at: DateTime,
-    #[schema(value_type = Option<String>, format = DateTime)]
     #[serde(serialize_with = "serialize_option_bson_datetime_as_rfc3339_string")]
     pub expires_at: Option<DateTime>,
 }
 
 #[derive(Serialize, Deserialize, Debug, utoipa::ToSchema)]
 pub struct User {
-    #[schema(value_type = String)]
     #[serde(serialize_with = "serialize_object_id_as_hex_string")]
     pub _id: ObjectId,
     pub username: String,
@@ -80,7 +70,6 @@ pub struct User {
     pub password: String,
     pub display_name: String,
     pub avatar_url: Option<String>,
-    #[schema(value_type = String, format = DateTime)]
     #[serde(serialize_with = "serialize_bson_datetime_as_rfc3339_string")]
     pub created_at: DateTime,
 }
